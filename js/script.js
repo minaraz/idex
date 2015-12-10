8b841b20681b4bf3158218eeddf2935ced46300b
Mina <minarazavi@Minas-MacBook-Pro.local>
2015-12-09 19:29:42 -0500

    where it's not Working anymore!

--- js/script.js
+++ js/script.js
@@ -4,13 +4,24 @@
 var menuButton = document.getElementById('menuButton');
 var menu = document.getElementById('menu');
 var homePage = document.getElementById('home');
+var aboutButton = document.getElementById('aboutButton');
+var servicesButton = document.getElementById('servicesButton');
+
+
+var about-us = document.getElementById('about-us');
 menuButton.onclick = function() {
   homePage.classList.remove('selected');
   menu.classList.add('selected');
+
 };
 // this will hide the menu//
 var hideMenu = function() {
   menu.classList.remove('selected');
   homePage.classList.add('selected');
+  about-us.classList.add('selected');
+
+
+}
+var hideAbout-us = function(){
 
 }
