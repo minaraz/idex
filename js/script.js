be91842771aee581669336ad2c5ce4c786097f1a
Mina <minarazavi@Minas-MacBook-Pro.local>
2015-12-09 19:44:57 -0500

    reverse the changes to CF js codes.

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ var menuButton = document.getElementById('menuButton');
 var menu = document.getElementById('menu');
 var homePage = document.getElementById('home');
 var aboutButton = document.getElementById('aboutButton');
-var servicesButton = document.getElementById('servicesButton');
-var about-us = document.getElementById('about-us');
 menuButton.onclick = function() {
   homePage.classList.remove('selected');
   menu.classList.add('selected');
@@ -16,10 +14,5 @@ menuButton.onclick = function() {
 var hideMenu = function() {
   menu.classList.remove('selected');
   homePage.classList.add('selected');
-  about-us.classList.add('selected');
-
-
-}
-var hideAbout-us = function(){
 
 }
