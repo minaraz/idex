dfd5ffaa813df5fbc87d0df9cc040108c5c73309
Mina <minarazavi@Minas-MacBook-Pro.local>
2015-12-13 14:20:59 -0500

    all pages are working!

--- js/script.js
+++ js/script.js
@@ -11,10 +11,44 @@ var backAbout = document.getElementById('back-about');
 var servicesButton = document.getElementById('servicesButton');
 var servicePage = document.getElementById('services');
 var backServices = document.getElementById('back-services');
-var Gallery = document.getElementById('pics-gallery');
+var contactButton = document.getElementById('contactButton');
 var Contact = document.getElementById('contact');
 var footer = document.getElementById('footer');
+var homelink = document.getElementById('homeBtn');
+var aboutlink = document.getElementById('aboutBtn');
+var servicelink = document.getElementById('serviceBtn');
+var contactlink = document.getElementById('contactBtn');
+var serviceGallery = document.getElementById('SD');
+var Gallery = document.getElementById('pics-gallery');
+var closeGallery = document.getElementById('closeGallery');
 
+serviceGallery.onclick = function() {
+  servicePage.classList.remove('selected');
+  Gallery.classList.add('selected');}
+
+closeGallery.onclick = function() {
+  Gallery.classList.remove('selected');
+  servicePage.classList.add('selected');}
+
+homelink.onclick = function() {
+  Contact.classList.remove('selected');
+  homePage.classList.add('selected');
+  footer.classList.add('selected');}
+
+aboutlink.onclick = function() {
+  Contact.classList.remove('selected');
+  aboutPage.classList.add('selected');
+  footer.classList.add('selected');}
+
+servicelink.onclick = function() {
+  Contact.classList.remove('selected');
+  servicePage.classList.add('selected');}
+
+contactButton.onclick = function() {
+  homePage.classList.remove('selected');
+  Contact.classList.add('selected');
+  footer.classList.add('selected');
+}
 servicesButton.onclick = function() {
   homePage.classList.remove('selected');
   services.classList.add('selected');
@@ -45,8 +79,6 @@ backAbout.onclick = function() {
   homePage.classList.add('selected');
   mobileMenu.classList.add('selected');
 };
-
-
 
 
 
